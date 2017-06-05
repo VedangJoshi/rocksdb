e43a5cec775158552380f81f6656f2deaa1e0ce2
Vedang Joshi <joshi.vedang94@gmail.com>
2017-06-04 19:57:21 -0700

    Final benchmark done

--- cmake-build-debug/build_version.cc
+++ cmake-build-debug/build_version.cc
@@ -1,4 +1,4 @@
 #include "build_version.h"
-const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:@942929901e075b6573882b479702b023b7998f23@";
-const char* rocksdb_build_git_date = "rocksdb_build_git_date:@2017/06/01 19:13:46@";
+const char* rocksdb_build_git_sha = "rocksdb_build_git_sha:@50c4d3b4db80fd557b476f5de2399ff333ea266a@";
+const char* rocksdb_build_git_date = "rocksdb_build_git_date:@2017/06/04 12:08:49@";
 const char* rocksdb_build_compile_date = __DATE__;
--- examples/multi-tier.cc
+++ examples/multi-tier.cc
@@ -1,9 +1,12 @@
+
 #include <cstdio>
 #include <string>
 #include <iostream>
 #include <vector>
 #include <string>
 #include <memory>
+#include <ctime>
+#include <chrono>
 
 #include "rocksdb/db.h"
 #include "db/db_impl.h"
@@ -12,10 +15,6 @@
 #include "rocksdb/options.h"
 #include "rocksdb/iterator.h"
 #include "util/mutexlock.h"
-#include "move_benchmark.h"
-
-#define TIER_SSD 0
-#define TIER_HDD 1
 
 using namespace rocksdb;
 using namespace std;
@@ -23,27 +22,6 @@ using namespace std;
 // Below are fake paths. Actual paths could be something like:  /dev/SSD1, /dev/HDD4 etc.
 std::string SSD_DB_path = "/tmp/SSD_on_rocks";
 std::string HDD_DB_path = "/tmp/HDD_on_rocks";
-
-vector<Slice> getKeyVector(int num_keys /* Total keys to be created */,
-                           int size_key /* Size of each key */) {
-    std::vector<Slice> keys;
-    auto iter = keys.begin();
-
-    // Create and populate the keys vector
-    for (int i = 1; i < num_keys; ++i) {
-        char* key = "key";
-        char* data_ = (char*) calloc(num_keys, size_key * sizeof(char));
-
-        memset(data_, 0, sizeof(char));
-        memmove(data_, key, sizeof(char) * 3);
-
-        char* ptr = data_ + (3 * sizeof(char));
-        data_ = strcat(data_, std::to_string(i).c_str());
-        data_ = strcat(data_, "\0");
-
-        keys.emplace_back(Slice(data_));
-    }
-}
 
 void printDB(DB* db, string message) {
     cout << message << endl;
@@ -65,8 +43,9 @@ int main(int argc, char** argv) {
     Status s;
     vector<Status> status_vec;
     vector<ColumnFamilyHandle*> cfh;
-    int num_keys = 10000;
+    int num_keys = 1;
     int key_size = 1; // in bytes
+    int val_size = 1; // in bytes
 
     if(argc < 3) {
         std::cout << "\n *** Incorrect arguments *** "
@@ -76,6 +55,7 @@ int main(int argc, char** argv) {
     } else {
         num_keys = atoi(argv[1]);
         key_size = atoi(argv[2]);
+        val_size = atoi(argv[3]);
     }
 
     /****************************************************************************************************************/
@@ -84,6 +64,7 @@ int main(int argc, char** argv) {
 
     // Holds normal KV pairs in hdd
     DB* db_hdd;
+
     /****************************************************************************************************************/
     std::vector<ColumnFamilyDescriptor> column_families_hdd;
 
@@ -99,6 +80,7 @@ int main(int argc, char** argv) {
         cerr << s.ToString() << endl;
         delete db_hdd;
     }
+
     /****************************************************************************************************************/
     std::vector<ColumnFamilyDescriptor> column_families_ssd;
 
@@ -114,51 +96,68 @@ int main(int argc, char** argv) {
         cerr << s.ToString() << endl;
         delete db_ssd;
     }
-    /****************************************************************************************************************/
-    // PUT - HDD
-    s = db_hdd->Put(WriteOptions(), handles_hdd[0], Slice("key1"), Slice("val1"));
 
-    // PUTS - SSD (atomic)
-    WriteBatch batch;
-    batch.Put(handles_ssd[0], Slice("key2"), Slice("val2"));
-    batch.Put(handles_ssd[0], Slice("key3"), Slice("val3"));
-    batch.Put(handles_ssd[0], Slice("key4"), Slice("val4"));
-    s = db_ssd->Write(WriteOptions(), &batch);
-    assert(s.ok());
     /****************************************************************************************************************/
 
     std::vector<Slice> keys;
+
+    // PUT - SSD
+    // Create and populate the keys vector
+    for (int i = 1; i < num_keys; ++i) {
+        char* key = "key";
+        char* key_data_ = (char*) calloc(num_keys, key_size * sizeof(char));
+        memset(key_data_, 0, sizeof(char));
+        memmove(key_data_, key, sizeof(char) * 3);
+        key_data_ = strcat(key_data_, std::to_string(i).c_str());
+        key_data_ = strcat(key_data_, "\0");
+
+        // Also add to the key vector, later used for moving keys around
+        keys.emplace_back(Slice(key_data_));
+
+        char* val = "val";
+        char* value_data_ = (char*) calloc(num_keys, val_size * sizeof(char));
+        memset(value_data_, 0, sizeof(char));
+        memmove(value_data_, val, sizeof(char) * 3);
+        value_data_ = strcat(value_data_, std::to_string(i).c_str());
+        value_data_ = strcat(value_data_, "\0");
+
+        //cout << key_data_ << " : " << value_data_ << endl;
+        s = db_ssd->Put(WriteOptions(), handles_hdd[0], Slice(key_data_), Slice(value_data_));
+        assert(s.ok());
+    }
+
+    /****************************************************************************************************************/
+
     std::vector<string> values;
-    auto iter = keys.begin();
 
     /*
      *  Benchmark
      */
-    keys = getKeyVector(num_keys);
 
-    printDB(db_hdd, "\nBefore :: HDD");
-    cout << endl;
-    printDB(db_ssd, "\nBefore :: SSD");
-    cout << endl;
+    //printDB(db_hdd, "\nBefore :: HDD");
+    //cout << endl;
+    //printDB(db_ssd, "\nBefore :: SSD");
+    //cout << endl;
 
-    cout << "*****" << endl;
+    //cout << "*****" << endl;
 
     // Time start
-    auto start = std::chrono::high_resolution_clock::now();
+    auto start = std::chrono::steady_clock::now();
+    //cout << "start: " << start.time_since_epoch();
 
     // MOVE
     // move(src_db, dest_db, dest_db_cf_handle, ReadOptions, WriteOptions, keys_vector)
     s = db_ssd->move(db_ssd, db_hdd, handles_hdd[0], ReadOptions(), WriteOptions(), keys);
 
     // Time end
-    auto end = std::chrono::high_resolution_clock::now() - start;
-    long long duration = std::chrono::duration_cast<std::chrono::microseconds> (end).count();
-    cout << duration << endl;
+    auto end = std::chrono::steady_clock::now();
 
-    printDB(db_hdd, "\nAfter :: HDD");
-    cout << endl;
-    printDB(db_ssd, "\nAfter :: SSD");
-    cout << endl;
+    cout << "Time: " << std::chrono::duration_cast<chrono::microseconds> (end - start).count() << endl;
+
+    // printDB(db_hdd, "\nAfter :: HDD");
+    // cout << endl;
+    // printDB(db_ssd, "\nAfter :: SSD");
+    // cout << endl;
 
     /****************************************************************************************************************/
     for (auto handle : handles_hdd) {
@@ -172,7 +171,6 @@ int main(int argc, char** argv) {
 
     delete db_hdd;
     delete db_ssd;
-
 
     return 0;
 }
